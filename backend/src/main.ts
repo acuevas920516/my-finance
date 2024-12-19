c77d4c6eaa5384b29cd7f45405e85afa7301b2d1
Alex <acuevas1605@gmail.com>
2024-12-19 20:29:11 +0100

    adding frontend and backend structure and fixing docker config

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My Finance API')
+    .setDescription('API for My Finance`s project')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
