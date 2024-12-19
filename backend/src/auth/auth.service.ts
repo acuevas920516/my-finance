0b5484dce6fd93a47dffdda63b8ab2bb2aa06338
Alex <acuevas1605@gmail.com>
2024-12-19 23:23:08 +0100

    add jwt config
    Adding jwt configuration, and updating auth module to return token instead user data

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+) {}
 
-    async signIn(email: string, password: string) {
-        const user = await this.userService.findOneByEmail(email);
-        if (user?.password !== password) {
-            return new UnauthorizedException();
-        }
-
-        return user;
+  async signIn(
+    email: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.userService.findOneByEmail(email);
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
     }
+
+    const payload = { sub: user.id, username: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
 }
