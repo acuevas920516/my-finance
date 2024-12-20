8b85347f82b0073563b5bb5d011346f7020ec730
Alex <acuevas1605@gmail.com>
2024-12-20 04:25:31 +0100

    adding passport logic (#5)
    adding passport logic and password serialization

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,9 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  username: string;
 
   @Column()
   firstName: string;
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,23 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private readonly userRepository: Repository<User>,
-    ) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
-    async findAll(): Promise<User[]> {
-        return this.userRepository.find();
-    }
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
 
-    async findOne(id: number): Promise<User> {
-        return this.userRepository.findOneBy({id});
-    }
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
 
-    async findOneByEmail(email: string): Promise<User> {
-        return this.userRepository.findOneBy({email: email});
-    }
+  async findOneByEmail(email: string): Promise<User> {
+    return this.userRepository.findOneBy({ email: email });
+  }
+
+  async findOneByUsername(username: string): Promise<User> {
+    return this.userRepository.findOneBy({ username: username });
+  }
+
+  async create(user: User): Promise<User> {
+    // Generate a salt
+    const salt = await bcrypt.genSalt();
+
+    const {password, ...data} = user;
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Save the user with the hashed password
+    const createdUser = this.userRepository.create({
+      ...data,
+      password: hashedPassword,
+    });
+    return this.userRepository.save(createdUser);
+  }
 }
